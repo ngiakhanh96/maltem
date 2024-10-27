3c8cb4fd7cfa8407d9a250a45e631955fad5f56b
WhiteK <khanh96.ng@gmail.com>
2024-10-27 19:08:03 +0800

    Remove seed code

--- Backend/MaltemBe/Program.cs
+++ Backend/MaltemBe/Program.cs
@@ -1,7 +1,6 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using MaltemBe.Converters;
-using MaltemBe.Models;
 using MaltemBe.Repositories;
 using MaltemBe.Repositories.Interfaces;
 using MaltemBe.Services;
@@ -40,54 +39,6 @@ builder.Services.AddCors(options =>
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<CafeDbContext>();
-    var employeeService = scope.ServiceProvider.GetRequiredService<IEmployeeService>();
-    await db.Database.MigrateAsync();
-    var cafesCount = await db.Cafes.CountAsync();
-    if (cafesCount == 0)
-    {
-        var starbucks = new Cafe
-        {
-            Name = "Starbucks",
-            Description = "Starbucks Coffee",
-            Location = "US",
-            Logo = []
-        };
-        starbucks.AddEmployee(
-            new Employee
-            {
-                Id = employeeService.GenerateEmployeeId(),
-                Name = "John Witch",
-                Email_address = "johnwitch@gmail.com",
-                Phone_number = 88888888,
-                Gender = Gender.Male
-            },
-            DateTime.UtcNow.AddDays(-2));
-        starbucks.AddEmployee(
-            new Employee
-            {
-                Id = employeeService.GenerateEmployeeId(),
-                Name = "Kati Witch",
-                Email_address = "katiwitch@gmail.com",
-                Phone_number = 99999999,
-                Gender = Gender.Female
-            },
-            DateTime.UtcNow.AddDays(-1));
-        var localCafe = new Cafe
-        {
-            Name = "Local cafe",
-            Description = "Local cafe",
-            Location = "UK",
-            Logo = []
-        };
-        await db.Cafes.AddAsync(starbucks);
-        await db.Cafes.AddAsync(localCafe);
-        await db.SaveChangesAsync();
-    }
-}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
